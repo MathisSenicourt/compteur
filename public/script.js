41446609641efab06efb3212a3fd285130a4b78e
MathisSenicourt <mathiscience@hotmail.fr>
2024-03-26 14:33:30 +0100

    compteur ok

--- public/script.js
+++ public/script.js
@@ -1,25 +1,107 @@
+async function getValueFirstTeamFromServer() {
+    try {
+        const response = await fetch('/getFirstTeamValue');
+        if (!response.ok) {
+            throw new Error('Erreur lors de la récupération de la valeur de la première équipe');
+        }
+        const data = await response.text();
+        console.log('Valeur de la première équipe :', data);
+        return data;
+    } catch (error) {
+        console.error('Erreur:', error);
+        throw error;
+    }
+}
+
+async function getValueSecondTeamFromServer() {
+    try {
+        const response = await fetch('/getSecondTeamValue');
+        if (!response.ok) {
+            throw new Error('Erreur lors de la récupération de la valeur de la deuxième équipe');
+        }
+        const data = await response.text();
+        console.log('Valeur de la deuxième équipe :', data);
+        return data;
+    } catch (error) {
+        console.error('Erreur:', error);
+        throw error;
+    }
+}
+
+async function setFirstTeamScoreOnServer(score) {
+    try {
+        const response = await fetch('/setFirstTeamScore/' + score);
+        if (!response.ok) {
+            throw new Error('Erreur lors de la définition du score de la première équipe');
+        }
+        console.log('Score de la première équipe défini avec succès');
+    } catch (error) {
+        console.error('Erreur:', error);
+        throw error;
+    }
+}
+
+async function setSecondTeamScoreOnServer(score) {
+    try {
+        const response = await fetch('/setSecondTeamScore/' + score);
+        if (!response.ok) {
+            throw new Error('Erreur lors de la définition du score de la deuxième équipe');
+        }
+        console.log('Score de la deuxième équipe défini avec succès');
+    } catch (error) {
+        console.error('Erreur:', error);
+        throw error;
+    }
+}
+
 async function incrementerValeurEquipe1(score) {
-    const currentValue = await getValueFirstTeamFromServer();
-    setFirstTeamScoreOnServer(parseInt(currentValue) + score);
-    document.getElementById("Score1").innerText = await getValueFirstTeamFromServer();
+    try {
+        const currentValue = await getValueFirstTeamFromServer();
+        const parsedValue = parseInt(currentValue);
+        if (!isNaN(parsedValue)) {
+            await setFirstTeamScoreOnServer(parsedValue + score);
+            document.getElementById("Score1").innerText = await getValueFirstTeamFromServer();
+        } else {
+            console.error('La valeur renvoyée par le serveur pour l\'équipe 1 n\'est pas un nombre valide');
+        }
+    } catch (error) {
+        console.error('Erreur:', error);
+    }
 }
 
 async function incrementerValeurEquipe2(score) {
-    const currentValue = await getValueSecondTeamFromServer();
-    setSecondTeamScoreOnServer(parseInt(currentValue) + score);
-    document.getElementById("Score2").innerText = await getValueSecondTeamFromServer();
+    try {
+        const currentValue = await getValueSecondTeamFromServer();
+        const parsedValue = parseInt(currentValue);
+        if (!isNaN(parsedValue)) {
+            await setSecondTeamScoreOnServer(parsedValue + score);
+            document.getElementById("Score2").innerText = await getValueSecondTeamFromServer();
+        } else {
+            console.error('La valeur renvoyée par le serveur pour l\'équipe 2 n\'est pas un nombre valide');
+        }
+    } catch (error) {
+        console.error('Erreur:', error);
+    }
 }
 
 async function reinitialiserValeurs() {
-    await setFirstTeamScoreOnServer(0);
-    await setSecondTeamScoreOnServer(0);
-    document.getElementById("Score1").innerText = await getValueFirstTeamFromServer();
-    document.getElementById("Score2").innerText = await getValueSecondTeamFromServer();
+    try {
+        await setFirstTeamScoreOnServer(0);
+        await setSecondTeamScoreOnServer(0);
+        document.getElementById("Score1").innerText = await getValueFirstTeamFromServer();
+        document.getElementById("Score2").innerText = await getValueSecondTeamFromServer();
+    } catch (error) {
+        console.error('Erreur:', error);
+    }
 }
 
 async function init() {
-    document.getElementById("Score1").innerText = await getValueFirstTeamFromServer();
-    document.getElementById("Score2").innerText = await getValueSecondTeamFromServer();
+    try {
+        document.getElementById("Score1").innerText = await getValueFirstTeamFromServer();
+        document.getElementById("Score2").innerText = await getValueSecondTeamFromServer();
+    } catch (error) {
+        console.error('Erreur:', error);
+    }
 }
 
 init();
@@ -34,69 +116,3 @@ document.getElementById("button6").addEventListener("click", () => incrementerVa
 document.getElementById("button7").addEventListener("click", () => incrementerValeurEquipe2(3));
 document.getElementById("button8").addEventListener("click", () => incrementerValeurEquipe2(-1));
 document.getElementById("lastButton").addEventListener("click", () => reinitialiserValeurs());
-
-function getValueFirstTeamFromServer() {
-    fetch('/getFirstTeamValue')
-        .then(async response => {
-            if (!response.ok) {
-                throw new Error('Erreur lors de la récupération de la valeur de la première équipe');
-            }
-            return response.text();
-        })
-        .then(data => {
-            console.log('Valeur de la première équipe :', data);
-            // Faites ce que vous voulez avec la valeur de la première équipe ici
-        })
-        .catch(error => {
-            console.error('Erreur:', error);
-        });
-}
-
-function getValueSecondTeamFromServer() {
-    fetch('/getSecondTeamValue')
-        .then(async response => {
-            if (!response.ok) {
-                throw new Error('Erreur lors de la récupération de la valeur de la deuxiéme équipe');
-            }
-            return response.text();
-        })
-        .then(data => {
-            console.log('Valeur de la deuxiéme équipe :', data);
-        })
-        .catch(error => {
-            console.error('Erreur:', error);
-        });
-}
-
-function setFirstTeamScoreOnServer(score) {
-    fetch('/setFirstTeamScore/' + score)
-        .then(async response => {
-            if (!response.ok) {
-                throw new Error('Erreur lors de la définition du score de la première équipe');
-            }
-            return response;
-        })
-        .then(data => {
-            console.log('Score de la première équipe défini avec succès');
-        })
-        .catch(error => {
-            console.error('Erreur:', error);
-        });
-}
-
-function setSecondTeamScoreOnServer(score) {
-    fetch('/setSecondTeamScore/' + score)
-        .then(async response => {
-            if (!response.ok) {
-                throw new Error('Erreur lors de la définition du score de la deuxième équipe');
-            }
-            return response;
-        })
-        .then(data => {
-            console.log('Score de la deuxième équipe défini avec succès');
-        })
-        .catch(error => {
-            console.error('Erreur:', error);
-        });
-}
-
--- server.js
+++ server.js
@@ -6,16 +6,13 @@ const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 const app = express();
 const port = 3000;
 
-// Serveur Express pour servir les fichiers statiques
 app.use(express.static('public'));
+app.use(express.json());
 
-// Route pour servir la page HTML
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
 });
 
-
-// Route pour appeler la fonction getValueFirstTeam et renvoyer la valeur de la première équipe
 app.get('/getFirstTeamValue', (req, res) => {
     getValueTeam(0, (err, value) => {
         if (err) {
@@ -26,10 +23,8 @@ app.get('/getFirstTeamValue', (req, res) => {
     });
 });
 
-
-// Route pour appeler la fonction getValueSecondTeam et renvoyer la valeur de la deuxième équipe
 app.get('/getSecondTeamValue', (req, res) => {
-    getValueTeam(1,(err, value) => {
+    getValueTeam(1, (err, value) => {
         if (err) {
             res.status(500).send("Erreur lors de la récupération de la valeur de la deuxième équipe");
         } else {
@@ -38,8 +33,8 @@ app.get('/getSecondTeamValue', (req, res) => {
     });
 });
 
-// Fonction pour récupérer la valeur de l'équipe passé en paramétre depuis le fichier score.csv
 function getValueTeam(team, callback) {
+    console.log("getValueTeam")
     const results = [];
     fs.createReadStream('score.csv')
         .pipe(csv())
@@ -47,18 +42,17 @@ function getValueTeam(team, callback) {
         .on('end', () => {
             if (results.length > team) {
                 const row = results[team];
-                const secondTeamValue = row[Object.keys(row)[0]];
-                callback(null, secondTeamValue);
+                const teamValue = row[Object.keys(row)[0]];
+                callback(null, teamValue);
             } else {
-                callback("Il n'y a pas "+team+" équipe dans le fichier CSV", null);
+                callback("Il n'y a pas d'équipe " + (team+1) + " dans le fichier CSV", null);
             }
         });
 }
 
-// Route pour définir le score de la première équipe
 app.get('/setFirstTeamScore/:score', (req, res) => {
     const score = req.params.score;
-    setTeamScore(0, score, (err, message) => {
+    updateValueTeam(0, score, (err, message) => {
         if (err) {
             res.status(500).send("Erreur lors de la définition du score de la première équipe : " + err);
         } else {
@@ -67,10 +61,9 @@ app.get('/setFirstTeamScore/:score', (req, res) => {
     });
 });
 
-// Route pour définir le score de la deuxième équipe
 app.get('/setSecondTeamScore/:score', (req, res) => {
     const score = req.params.score;
-    setTeamScore(1, score, (err, message) => {
+    updateValueTeam(1, score, (err, message) => {
         if (err) {
             res.status(500).send("Erreur lors de la définition du score de la deuxième équipe : " + err);
         } else {
@@ -79,35 +72,30 @@ app.get('/setSecondTeamScore/:score', (req, res) => {
     });
 });
 
-
-// Fonction pour définir le score de l'équipe passé en paramètre dans le fichier score.csv
-function setTeamScore(team, newScore, callback) {
+function updateValueTeam(team, newValue, callback) {
+    console.log("updateValueTeam " + team+" = value : "+newValue);
     const results = [];
     fs.createReadStream('score.csv')
         .pipe(csv())
         .on('data', (data) => results.push(data))
         .on('end', () => {
-            if (results.length > team) {
-                results[team][Object.keys(results[team])[0]] = newScore;
-                const csvWriter = createCsvWriter({
-                    path: 'score.csv',
-                    header: [{id: Object.keys(results[team])[0], title: Object.keys(results[team])[0]}]
+            if (results.length >= team) {
+                const row = results[team];
+                const key = Object.keys(row)[0];
+                row[key] = newValue;
+                const ws = fs.createWriteStream('score.csv');
+                ws.write(Object.keys(results[0]).join(',') + '\n');
+                results.forEach((data) => {
+                    ws.write(Object.values(data).join(',') + '\n');
                 });
-                csvWriter.writeRecords(results)
-                    .then(() => {
-                        callback(null, "Score défini avec succès pour l'équipe " + (team+1));
-                    })
-                    .catch((error) => {
-                        callback(error, null);
-                    });
+                ws.end();
+                callback(null, "La valeur de l'équipe " + team + " a été mise à jour avec succès.");
             } else {
-                callback("Il n'y a pas " + team + " équipe dans le fichier CSV", null);
+                callback("Il n'y a pas d'équipe " + team + " dans le fichier CSV", null);
             }
         });
 }
 
-
-// Démarrez le serveur
 app.listen(port, () => {
     console.log(`Le serveur est lancé sur http://localhost:${port}`);
 });
